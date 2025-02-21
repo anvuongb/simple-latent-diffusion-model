9276cd789b4f45697703b03d0fe3632f23595adb
김성원 <kssw888@naver.com>
2025-02-21 14:56:14 +0900

    update base unet

--- simple-latent-diffusion-model/diffusion_model/network/base_unet.py
+++ simple-latent-diffusion-model/diffusion_model/network/base_unet.py
@@ -1,9 +1,18 @@
 import torch
 import torch.nn as nn
 
-
-
 class BaseUnet(nn.Module):
-    def __init__(self, ):
+    def __init__(
+        self, 
+        dim,
+        dim_mults = [1, 2, 4, 8],
+        channels : int = 3):
         super().__init__()
-        pass
+
+        self.channels = channels
+        self.dim = dim
+        
+        self.init_conv = nn.Conv2d(self.channels, self.dim, 7, padding = 3)
+
+        dims = [self.dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
