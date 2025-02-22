4a6824c29f960e43b70cbd2ef0881fc71184d10c
김성원 <kssw888@naver.com>
2025-02-22 22:53:39 +0900

    Made BaseUnet

--- simple-latent-diffusion-model/diffusion_model/network/attention.py
+++ simple-latent-diffusion-model/diffusion_model/network/attention.py
@@ -1,6 +1,22 @@
-class my_class(object):
-    pass
+import torch
+import torch.nn as nn
 
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        heads = 4,
+        dim_head = 32,
+    ):
+        super().__init__()
+        self.heads = heads
+        hidden_dim = dim_head * heads
 
+        self.norm = nn.RMSNorm(dim)
+        self.attn = nn.MultiheadAttention(
+            
+            )
 
-
+    def forward(self, x):
+        b, _, h, w = x.shape
+        
