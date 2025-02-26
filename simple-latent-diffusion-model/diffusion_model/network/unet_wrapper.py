99c00eab8b20499d588994c4c86b1ee1fe87b21d
김성원 <kssw888@naver.com>
2025-02-26 19:47:20 +0900

    Update the unet_wrapper

--- simple-latent-diffusion-model/diffusion_model/network/unet_wrapper.py
+++ simple-latent-diffusion-model/diffusion_model/network/unet_wrapper.py
@@ -12,14 +12,13 @@ class UnetWrapper(nn.Module):
         self.add_module('network', Unet(**config))
 
         # ConditionalEncoder
-        if cond_encoder is None:
-            cond_encoder = nn.Identity()
         self.add_module('cond_encoder', cond_encoder)
         
     def forward(self, x, t, y=None):
         if t.dim() == 0:
             t = x.new_full((x.size(0), ), t, dtype = torch.int, device = x.device)
         if y is not None:
+            assert self.cond_encoder is not None, 'You need to set ConditionalEncoder for conditional sampling.'
             if torch.is_tensor(y) == False:
                 y = torch.tensor([y], device=x.device)
             if y.size(0) != x.size(0):
