529cd062114d772540aa06d6f4a44d53bf40734c
김성원 <kssw888@naver.com>
2025-02-26 19:52:11 +0900

    update cond_encoder

--- simple-latent-diffusion-model/helper/cond_encoder.py
+++ simple-latent-diffusion-model/helper/cond_encoder.py
@@ -1,14 +1,19 @@
 import torch
 import torch.nn as nn
+import yaml
 
 class ConditionEncoder(nn.Module):
     def __init__(
         self, 
-        cond_type : str,
-        num_cond : int,
-        embed_dim: int = 128,
+        config_path
         ):
         super().__init__()
+        with open(config_path, "r") as file:
+            config = yaml.safe_load(file)['cond_encoder']
+        cond_type = config['cond_type']
+        num_cond = config['num_cond']
+        embed_dim = config['embed_dim']
+
         if cond_type == 'class':
             self.embed = nn.Embedding(num_cond, embed_dim)
         elif cond_type == 'numeric':
